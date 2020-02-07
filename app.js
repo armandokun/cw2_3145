22e43c69116d5a96181edb68bbadaa72cb9f3ce7
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 10:45:22 +0000

    PWA: Display a place-holder image first...

--- app.js
+++ app.js
@@ -3,33 +3,53 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
 }
 
-let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
+// Notifications
+var button = document.getElementById("notifications");
+button.addEventListener('click', function (e) {
+    Notification.requestPermission().then(function (result) {
+        if (result === 'granted') {
+            randomNotification();
+        }
+    });
+});
+
+function randomNotification() {
+    var randomItem = Math.floor(Math.random() * images.length);
+    var notifTitle = images[randomItem].name;
+    var notifBody = 'Created by Armando';
+    var notifImg = 'img/' + images[randomItem].name + '.png';
+    var options = {
+        body: notifBody,
+        icon: notifImg
+    };
+
+    var notif = new Notification(notifTitle, options);
+    setTimeout(randomNotification, 30000);
+}
+
+// Progressive loading images
+var imagesToLoad = document.querySelectorAll('img[data-src]');
+var loadImages = function (image) {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
+    image.onload = function () {
         image.removeAttribute('data-src');
     };
 };
 
-// loop to load images
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
-
 if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
+    var observer = new IntersectionObserver(function (items, observer) {
+        items.forEach(function (item) {
             if (item.isIntersecting) {
                 loadImages(item.target);
                 observer.unobserve(item.target);
             }
         });
     });
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach(function (img) {
         observer.observe(img);
     });
 } else {
-    imagesToLoad.forEach((img) => {
+    imagesToLoad.forEach(function (img) {
         loadImages(img);
     });
 }
