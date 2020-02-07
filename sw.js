34c422b394538a29eeccbfad492162b6fa618d0f
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 08:15:15 +0000

    PWA: Testing

--- sw.js
+++ sw.js
@@ -3,18 +3,20 @@ self.importScripts('img/data.js');
 // Files to cache
 var cacheName = 'armandokun-v1';
 var appShellFiles = [
-    '/js/',
-    '/js/vue.js',
-    '/js/userForm.js',
-    '/js/provider.js',
-    '/index.html',
-    '/css/styles.css'
+    '/cw2_3145/js/',
+    '/cw2_3145/js/vue.js',
+    '/cw2_3145/js/userForm.js',
+    '/cw2_3145/js/provider.js',
+    '/cw2_3145/index.html',
+    '/cw2_3145/css/styles.css'
 ];
 
 var courseImages = [];
 for (var i = 0; i < images.length; i++) {
     courseImages.push('img/' + images[i].name + '.png');
 }
+
+console.log(courseImages)
 
 var contentToCache = appShellFiles.concat(courseImages);
 
