09f3c0997268c52f2a985763fb852f565213da7b
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 13:59:41 +0000

    PWA: Notification when cache is created

--- app.js
+++ app.js
@@ -12,20 +12,22 @@ const button = document.getElementById("notifications");
 button.addEventListener('click', function (e) {
     Notification.requestPermission().then(function (result) {
         if (result === 'granted') {
-            randomNotification();
+            navigator.serviceWorker.getRegistration()
+                .then(reg => reg.showNotification('Cache has been created'))
         }
     });
 });
 
 function randomNotification() {
-    const randomItem = Math.floor(Math.random() * images.length);
-    const notifTitle = images[randomItem].name;
-    const notifBody = 'Created by Armando';
-    const notifImg = 'img/' + images[randomItem].name + '.png';
-    const options = {
-        body: notifBody,
-        icon: notifImg
-    };
+
+    // const randomItem = Math.floor(Math.random() * images.length);
+    // const notifTitle = images[randomItem].name;
+    // const notifBody = 'Created by Armando';
+    // const notifImg = 'img/' + images[randomItem].name + '.png';
+    // const options = {
+    //     body: notifBody,
+    //     icon: notifImg
+    // };
 
     var notif = new Notification(notifTitle, options);
     setTimeout(randomNotification, 30000);
