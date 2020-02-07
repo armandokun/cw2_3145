c06499fca17cb79827b9b61cd1a3ece2e98377d9
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 13:39:46 +0000

    PWA: Dynamic SW added

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ self.importScripts('img/data.js');
 
 // Files to cache
 const cacheName = 'armandokun-v2';
+const dynamicCache = 'armandokun-dynamic-v1';
 const appShellFiles = [
     '/cw2_3145/js/vue.js',
     '/cw2_3145/js/userForm.js',
@@ -44,7 +45,7 @@ self.addEventListener('fetch', function (e) {
         caches.match(e.request).then(function (r) {
             console.log('[Service Worker] Fetching resource: ' + e.request.url);
             return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
+                return caches.open(dynamicCache).then(function (cache) {
                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
                     cache.put(e.request, response.clone());
                     return response;
