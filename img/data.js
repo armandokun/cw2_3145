e5e0b6da7a96655df3dd10f91fa31a5685badba5
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 07:33:24 +0000

    PWA: SW has all files to cache

---
+++ img/data.js
@@ -0,0 +1,38 @@
+let images = [
+    {
+        name: "Accounting"
+    },
+    {
+        name: "Accounting-Management"
+    },
+    {
+        name: "Biology"
+    },
+    {
+        name: "Computer-Science"
+    },
+    {
+        name: "Dance"
+    },
+    {
+        name: "Finance"
+    },
+    {
+        name: "Guitar"
+    },
+    {
+        name: "Piano"
+    },
+    {
+        name: "Project-Management"
+    },
+    {
+        name: "test1"
+    },
+    {
+        name: "test2"
+    },
+    {
+        name: "Violin"
+    },
+];
--- sw.js
+++ sw.js
@@ -1,56 +1,43 @@
-// self.importScripts('img/games.js');
-//
-// // Files to cache
-// var cacheName = 'js13kPWA-v1';
-// var appShellFiles = [
-//     '/pwa-examples/js13kpwa/',
-//     '/pwa-examples/js13kpwa/index.html',
-//     '/pwa-examples/js13kpwa/app.js',
-//     '/pwa-examples/js13kpwa/style.css',
-//     '/pwa-examples/js13kpwa/fonts/graduate.eot',
-//     '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-//     '/pwa-examples/js13kpwa/fonts/graduate.woff',
-//     '/pwa-examples/js13kpwa/favicon.ico',
-//     '/pwa-examples/js13kpwa/img/js13kgames.png',
-//     '/pwa-examples/js13kpwa/img/bg.png',
-//     '/pwa-examples/js13kpwa/icons/icon-32.png',
-//     '/pwa-examples/js13kpwa/icons/icon-64.png',
-//     '/pwa-examples/js13kpwa/icons/icon-96.png',
-//     '/pwa-examples/js13kpwa/icons/icon-128.png',
-//     '/pwa-examples/js13kpwa/icons/icon-168.png',
-//     '/pwa-examples/js13kpwa/icons/icon-192.png',
-//     '/pwa-examples/js13kpwa/icons/icon-256.png',
-//     '/pwa-examples/js13kpwa/icons/icon-512.png'
-// ];
-// var gamesImages = [];
-// for(var i=0; i<games.length; i++) {
-//     gamesImages.push('data/img/'+games[i].slug+'.jpg');
-// }
-// var contentToCache = appShellFiles.concat(gamesImages);
-//
-// // Installing Service Worker
-// self.addEventListener('install', function(e) {
-//     console.log('[Service Worker] Install');
-//     e.waitUntil(
-//         caches.open(cacheName).then(function(cache) {
-//             console.log('[Service Worker] Caching all: app shell and content');
-//             return cache.addAll(contentToCache);
-//         })
-//     );
-// });
-//
-// // Fetching content using Service Worker
-// self.addEventListener('fetch', function(e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function(r) {
-//             console.log('[Service Worker] Fetching resource: '+e.request.url);
-//             return r || fetch(e.request).then(function(response) {
-//                 return caches.open(cacheName).then(function(cache) {
-//                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
-//                     cache.put(e.request, response.clone());
-//                     return response;
-//                 });
-//             });
-//         })
-//     );
-// });
+self.importScripts('img/data.js');
+
+// Files to cache
+var cacheName = 'armandokun-v1';
+var appShellFiles = [
+    '/cw2_3145/js/',
+    '/cw2_3145/index.html',
+    '/cw2_3145/css/styles.css'
+];
+
+var courseImages = [];
+for (var i = 0; i < images.length; i++) {
+    courseImages.push('data/img/' + images[i].name + '.png');
+}
+var contentToCache = appShellFiles.concat(courseImages);
+
+// Installing Service Worker
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(contentToCache);
+        })
+    );
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone()).then(res => {
+                        return res
+                    });
+                });
+            });
+        })
+    );
+});
