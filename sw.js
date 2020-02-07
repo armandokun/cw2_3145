559ea13088d8751d644d128e9d7895083dba3372
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 08:10:54 +0000

    PWA: sw.js amendment

--- sw.js
+++ sw.js
@@ -14,35 +14,17 @@ var appShellFiles = [
 var courseImages = [];
 for (var i = 0; i < images.length; i++) {
     courseImages.push('img/' + images[i].name + '.png');
-    console.log(images[i])
-    console.log(images[i].name)
 }
 
 var contentToCache = appShellFiles.concat(courseImages);
 
-// Installing Service Worker
-self.addEventListener('install', function (e) {
-    console.log('[Service Worker: ' + cacheName + '] Install');
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            console.log('[Service Worker: ' + cacheName + '] Caching all: app shell and content');
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
             return cache.addAll(contentToCache);
         })
     );
 });
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
