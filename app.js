9c1f9b540c3c6889228a7dad075e12c186f79016
armandokun <armandasvaicikauskas@gmail.com>
2020-02-07 09:10:14 +0000

    PWA: Notifications amendment

--- app.js
+++ app.js
@@ -2,26 +2,3 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
 }
-
-var button = document.getElementById("notifications");
-button.addEventListener('click', function (e) {
-    Notification.requestPermission().then(function (result) {
-        if (result === 'granted') {
-            randomNotification();
-        }
-    });
-});
-
-function randomNotification() {
-    var randomItem = Math.floor(Math.random() * images.length);
-    var notifTitle = images[randomItem].name;
-    var notifBody = 'Created by Armando';
-    var notifImg = 'img/' + images[randomItem].name + '.jpg';
-    var options = {
-        body: notifBody,
-        icon: notifImg
-    };
-
-    var notif = new Notification(notifTitle, options);
-    setTimeout(randomNotification, 30000);
-}
